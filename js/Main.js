a55be85e9f1803686d6bf7376d5034cd78af317b
ltrn <erichquerner@web.de>
2016-04-18 00:12:47 +0200

    add js lib

---
+++ js/Main.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+	
+	var game = new Phaser.Game(1200, 800, Phaser.AUTO,'',{ preload: preload, create: create, update: update });
+	function preload(){
+		
+	}
+	
+	function create(){
+		
+	}
+	
+	function update(){
+		
+	}
+};
